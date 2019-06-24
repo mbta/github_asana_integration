5c6fd17e4994544e888abccd16515fb4eade03ab
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2019-06-24 16:33:59 -0400

    id -> gid

--- app/handler.py
+++ app/handler.py
@@ -19,10 +19,10 @@ asana_url = 'https://app.asana.com/api/1.0/tasks'
 token = os.environ['ASANA_API_TOKEN']
 hook_secret_key = os.environb[b'GITHUB_SECRET']
 
-not_started_id = config.getint('DEFAULT', 'not_started_id')
-in_dev_id = config.getint('DEFAULT', 'in_dev_id')
-in_pr_id = config.getint('DEFAULT', 'in_pr_id')
-merged_done_id = config.getint('DEFAULT', 'merged_done_id')
+not_started_id = config.get('DEFAULT', 'not_started_id')
+in_dev_id = config.get('DEFAULT', 'in_dev_id')
+in_pr_id = config.get('DEFAULT', 'in_pr_id')
+merged_done_id = config.get('DEFAULT', 'merged_done_id')
 
 
 def handler(event, context):  # pylint:disable=unused-argument
@@ -99,11 +99,11 @@ def add_github_link(task, url):
         lambda field: field['name'] == 'GitHub PR', task['custom_fields'])
     if github_field and github_field['text_value'] != url:
         data = {'data': {'custom_fields': {}}}
-        data['data']['custom_fields'][github_field['id']] = url
+        data['data']['custom_fields'][github_field['gid']] = url
         requests.put("{}/{}".format(asana_url,
-                                    task['id']), headers=json_headers(), json=data)
+                                    task['gid']), headers=json_headers(), json=data)
         logger.info("updating github field %s with %s",
-                    github_field['id'], url)
+                    github_field['gid'], url)
 
 
 def confirm_project(task, project_id):
@@ -111,12 +111,12 @@ def confirm_project(task, project_id):
         return True
     raise Exception(
         "Task {} is not on the project board {} in Not Started, in Dev, or in PR"
-        .format(task['id'], project_id))
+        .format(task['gid'], project_id))
 
 
 def confirm_member(member, project_id):
-    if member['project']['id'] == project_id:
-        if (member['section']['id'] in [not_started_id, in_dev_id, in_pr_id]):
+    if member['project']['gid'] == project_id:
+        if (member['section']['gid'] in [not_started_id, in_dev_id, in_pr_id]):
             return True
     return False
 
@@ -129,7 +129,7 @@ def update_project(task, project_id, action, pr):
 
 
 def add_section(task, project_id, action, pr):
-    task_id = task['id']
+    task_id = task['gid']
     if action in ('opened', 'edited'):
         do_add_section(task_id, project_id, in_pr_id)
     elif action == 'closed' and pr['merged']:
